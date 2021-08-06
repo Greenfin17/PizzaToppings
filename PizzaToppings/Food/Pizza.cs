42b9518143fc19271c5c2490d44cdee0bbd47991
John Maple <jmaple@bellsouth.net>
2021-08-06 09:43:27 -0500

    Add project files.

---
+++ PizzaToppings/Food/Pizza.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaToppings.Food
+{
+    class Pizza
+    {
+        public IList<string> toppings { get; set; }
+
+        /*
+        public Pizza(IList<string> toppingList)
+        {
+            toppings= new List<string>();
+            foreach(var topping in toppingList)
+            {
+                toppings.Add(topping);
+            }
+        }
+        */
+    }
+}
---
+++ PizzaToppings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using PizzaToppings.Food;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace PizzaToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           List<Pizza> pizzaList = new List<Pizza>();
+           List<Pizza> pizzaList2 = new List<Pizza>();
+
+           string pizzaStr = new String(File.ReadAllText(@"..\..\..\Data\pizzas.json"));
+
+           // Pizza pizzaObj= JsonConvert.DeserializeObject<Pizza>(pizzaJson);
+           pizzaList = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"..\..\..\Data\pizzas.json"));
+
+            using (StreamReader file = File.OpenText(@"..\..\..\Data\pizzas.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                pizzaList2 = (List<Pizza>)serializer.Deserialize(file, typeof(List<Pizza>));
+            }
+
+            foreach(var pizza in pizzaList)
+            {
+                Console.WriteLine(String.Join(", ", pizza.toppings));
+            }
+            foreach(var pizza in pizzaList2)
+            {
+                Console.WriteLine(pizza);
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
